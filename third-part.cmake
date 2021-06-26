5365333c37b194ea03451215ab9ffdf298e74558
dieseldong <dieseldong@tencent.com>
2021-06-26 14:13:43 +0800

    添加lua库，并且自动化添加新库

--- third-part.cmake
+++ third-part.cmake
@@ -1,6 +1,18 @@
 
 function(UseThirdPart)
-	include("${CMAKE_CURRENT_FUNCTION_LIST_DIR}/src/glew-2.1.0/UseGlew.cmake")
-	include("${CMAKE_CURRENT_FUNCTION_LIST_DIR}/src/glfw-3.34/UseGlfw.cmake")
-	include("${CMAKE_CURRENT_FUNCTION_LIST_DIR}/src/glad/UseGlad.cmake")
+	file(GLOB_RECURSE children LIST_DIRECTORIES true ${CMAKE_CURRENT_SOURCE_DIR}/src/*)
+	set(dirs "")
+	foreach(item ${children})
+	if(IS_DIRECTORY ${item} AND EXISTS "${item}/Use.cmake")
+		list(APPEND dirs ${item})
+		include("${item}/Use.cmake")
+	endif()
+	endforeach()
+	foreach(dir ${dirs})
+		set(libraryName "")
+		get_filename_component(libraryName ${dir} NAME)
+		message(STATUS "Usagable Library - ${libraryName}")
+	endforeach()
 endfunction()
+
+
