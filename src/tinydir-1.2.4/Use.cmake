01402f3a1a106ef55355600d6a24c3bc4ae69b5e
dieseldong <dieseldong@tencent.com>
2021-06-27 16:49:20 +0800

    更新

---
+++ src/tinydir-1.2.4/Use.cmake
@@ -0,0 +1,7 @@
+
+function(UseTinyDir targetName)
+
+	message(STATUS "${targetName}  include ${CMAKE_CURRENT_FUNCTION_LIST_DIR}")
+	target_include_directories(${targetName} PUBLIC "${CMAKE_CURRENT_FUNCTION_LIST_DIR}")
+
+endfunction()
--- src/tinyxml/Use.cmake
+++ src/tinyxml/Use.cmake
@@ -1,7 +1,10 @@
 
-function(UseTinyDir targetName)
+function(UseTinyXml targetName)
 
 	message(STATUS "${targetName}  include ${CMAKE_CURRENT_FUNCTION_LIST_DIR}")
 	target_include_directories(${targetName} PUBLIC "${CMAKE_CURRENT_FUNCTION_LIST_DIR}")
+
+	message(STATUS "${targetName} add source ${CMAKE_CURRENT_FUNCTION_LIST_DIR}/tinyxml2.cpp")
+	target_sources(${targetName} PRIVATE "${CMAKE_CURRENT_FUNCTION_LIST_DIR}/tinyxml2.cpp")
 
 endfunction()
