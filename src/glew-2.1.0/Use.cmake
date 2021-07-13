604255112b7fe9459b689b5c9e29694c97ff1046
dieseldong <vgvgvvvdc@gmail.com>
2021-07-13 20:38:51 +0800

    更新

--- src/glew-2.1.0/Use.cmake
+++ src/glew-2.1.0/Use.cmake
@@ -8,4 +8,10 @@ function(UseGlew targetName)
 	target_link_directories(${targetName} PUBLIC "${CMAKE_CURRENT_FUNCTION_LIST_DIR}/lib/Release/x64")
 	target_link_libraries(${targetName} PUBLIC "${CMAKE_CURRENT_FUNCTION_LIST_DIR}/lib/Release/x64/glew32.lib")
 
+	add_custom_command(TARGET ${targetName} POST_BUILD
+    COMMAND ${CMAKE_COMMAND} -E copy_if_different  
+        "${CMAKE_CURRENT_FUNCTION_LIST_DIR}/bin/Release/x64/glew32.dll"
+        $<TARGET_FILE_DIR:${targetName}>)
+
+
 endfunction()
