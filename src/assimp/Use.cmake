2aaf20a49039221543d738bb3109be2c3fbbfbbc
dieseldong <dieseldong@tencent.com>
2021-07-23 08:53:44 +0800

    更新

---
+++ src/assimp/Use.cmake
@@ -0,0 +1,18 @@
+
+function(UseAssimp targetName)
+
+	message(STATUS "${targetName}  include ${CMAKE_CURRENT_FUNCTION_LIST_DIR}/include")
+	target_include_directories(${targetName} PUBLIC "${CMAKE_CURRENT_FUNCTION_LIST_DIR}/include")
+
+	message(STATUS "${targetName} link ${CMAKE_CURRENT_FUNCTION_LIST_DIR}/lib/")
+	target_link_directories(${targetName} PUBLIC "${CMAKE_CURRENT_FUNCTION_LIST_DIR}/lib/")
+	target_link_libraries(${targetName} PUBLIC "${CMAKE_CURRENT_FUNCTION_LIST_DIR}/lib/assimp-vc142-mt.lib")
+
+	add_definitions(-DASSIMP_DLL)
+
+	add_custom_command(TARGET ${targetName} POST_BUILD
+    COMMAND ${CMAKE_COMMAND} -E copy_if_different  
+        "${CMAKE_CURRENT_FUNCTION_LIST_DIR}/lib/assimp-vc142-mt.dll"
+        $<TARGET_FILE_DIR:${targetName}>)
+
+endfunction()
