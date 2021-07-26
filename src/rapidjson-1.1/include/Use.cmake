1f0516c4005fbcb12463b7ce562fb28ad0394070
dieseldong <dieseldong@tencent.com>
2021-07-26 18:18:29 +0800

    更新

--- src/rapidjson-1.1/include/Use.cmake
+++ src/rapidjson-1.1/include/Use.cmake
@@ -4,4 +4,6 @@ function(UseRapidJson targetName)
 	message(STATUS "${targetName}  include ${CMAKE_CURRENT_FUNCTION_LIST_DIR}/include")
 	target_include_directories(${targetName} PUBLIC "${CMAKE_CURRENT_FUNCTION_LIST_DIR}/include")
 
+	add_definitions(-DRAPIDJSON_HAS_STDSTRING=1)
+
 endfunction()
