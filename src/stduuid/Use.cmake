9d3a59360fb28b1cf24ee45316f9d84ba5fc38cd
dieseldong <dieseldong@tencent.com>
2021-07-28 17:03:37 +0800

    更新

--- src/stduuid/Use.cmake
+++ src/stduuid/Use.cmake
@@ -4,6 +4,6 @@ function(UseUUID targetName)
 	message(STATUS "${targetName}  include ${CMAKE_CURRENT_FUNCTION_LIST_DIR}")
 	target_include_directories(${targetName} PUBLIC "${CMAKE_CURRENT_FUNCTION_LIST_DIR}")
 
-	add_definitions(-DUUID_SYSTEM_GENERATOR)
+	target_compile_definitions(${targetName} PUBLIC UUID_SYSTEM_GENERATOR)
 
 endfunction()
