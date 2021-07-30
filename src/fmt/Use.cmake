57143af18a4591872e6e8fb958d6031d118494d2
dieseldong <vgvgvvvdc@gmail.com>
2021-07-31 03:55:23 +0800

    更新

--- src/fmt/Use.cmake
+++ src/fmt/Use.cmake
@@ -3,7 +3,7 @@ function(UseFmt targetName)
 
 	message(STATUS "${targetName}  include ${CMAKE_CURRENT_FUNCTION_LIST_DIR}/include")
 	message(STATUS "${targetName} link ${CMAKE_CURRENT_FUNCTION_LIST_DIR}/libs/x64-Release")
-	set(moduleName CmdLine)
+	set(moduleName Fmt)
 	ReMake_AddCustomModule(
 		TARGET_NAME ${targetName}
 		MODULE_NAME ${moduleName}
