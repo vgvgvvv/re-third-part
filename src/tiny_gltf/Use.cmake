a68c0d8c88f91d76488d197f04861726ad9e2bea
dieseldong <dieseldong@tencent.com>
2021-09-13 17:59:24 +0800

    添加Gltf

---
+++ src/tiny_gltf/Use.cmake
@@ -0,0 +1,13 @@
+
+function(UseTinyGltf targetName)
+
+	message(STATUS "${targetName}  include ${CMAKE_CURRENT_FUNCTION_LIST_DIR}")
+
+	set(moduleName TinyGltf)
+	ReMake_AddCustomModule(
+		TARGET_NAME ${targetName}
+		MODULE_NAME ${moduleName}
+		INC "${CMAKE_CURRENT_FUNCTION_LIST_DIR}"
+	)
+
+endfunction()
