79a7f8b1489f3b2cf956921b02ed25bdc83d6fb5
dieseldong <dieseldong@tencent.com>
2021-09-14 17:52:27 +0800

    更新

--- src/directx/Use.cmake
+++ src/directx/Use.cmake
@@ -1,8 +1,35 @@
 
+
+function(UseDirectX11 targetName)
+
+	message(STATUS "${targetName}  include ${CMAKE_CURRENT_FUNCTION_LIST_DIR}/DirectX/Include")
+	message(STATUS "${targetName} add lib ${CMAKE_CURRENT_FUNCTION_LIST_DIR}/DirectX/Lib/x64/")
+
+	set(moduleName DirectX12)
+	ReMake_AddCustomModule(
+		TARGET_NAME ${targetName}
+		MODULE_NAME ${moduleName}
+		INC "${CMAKE_CURRENT_FUNCTION_LIST_DIR}/DirectX/Include"
+		LIB "${CMAKE_CURRENT_FUNCTION_LIST_DIR}/DirectX/Lib/x64/dxgi.lib"
+			"${CMAKE_CURRENT_FUNCTION_LIST_DIR}/DirectX/Lib/x64/d3d9.lib"
+			"${CMAKE_CURRENT_FUNCTION_LIST_DIR}/DirectX/Lib/x64/d3d11.lib"
+			"${CMAKE_CURRENT_FUNCTION_LIST_DIR}/DirectX/Lib/x64/dxguid.lib"
+			"${CMAKE_CURRENT_FUNCTION_LIST_DIR}/DirectX/Lib/x64/d3dcompiler.lib"
+			"${CMAKE_CURRENT_FUNCTION_LIST_DIR}/DirectX/Lib/x64/d3dx11.lib"
+			"${CMAKE_CURRENT_FUNCTION_LIST_DIR}/DirectX/Lib/x64/dinput8.lib"
+			"${CMAKE_CURRENT_FUNCTION_LIST_DIR}/DirectX/Lib/x64/X3DAudio.lib"
+			"${CMAKE_CURRENT_FUNCTION_LIST_DIR}/DirectX/Lib/x64/xapobase.lib"
+			"${CMAKE_CURRENT_FUNCTION_LIST_DIR}/DirectX/Lib/x64/XAPOFX.lib"
+		DEFINE "WITH_D3DX_LIBS=1"
+	)
+
+endfunction()
+
+
 function(UseDirectX12 targetName)
 
-	message(STATUS "${targetName}  include ${CMAKE_CURRENT_FUNCTION_LIST_DIR}/Include")
-	message(STATUS "${targetName} add lib ${CMAKE_CURRENT_FUNCTION_LIST_DIR}/Lib/x64/")
+	message(STATUS "${targetName}  include ${CMAKE_CURRENT_FUNCTION_LIST_DIR}/DirectX/Include")
+	message(STATUS "${targetName} add lib ${CMAKE_CURRENT_FUNCTION_LIST_DIR}/DirectX/Lib/x64/")
 
 	set(moduleName DirectX12)
 	ReMake_AddCustomModule(
@@ -10,37 +37,14 @@ function(UseDirectX12 targetName)
 		MODULE_NAME ${moduleName}
 		INC "${CMAKE_CURRENT_FUNCTION_LIST_DIR}/DirectX/Include" 
 			"${CMAKE_CURRENT_FUNCTION_LIST_DIR}/D3DX12/Include" 
-		LIB "${CMAKE_CURRENT_FUNCTION_LIST_DIR}/Lib/x64/d2d12.lib"
+		LIB "${CMAKE_CURRENT_FUNCTION_LIST_DIR}/DirectX/Lib/x64/d3d12.lib"
 	)
 
 	add_custom_command(TARGET ${targetName} POST_BUILD
     COMMAND ${CMAKE_COMMAND} -E copy_if_different  
-        "${CMAKE_CURRENT_FUNCTION_LIST_DIR}/Lib/x64/d2d12.dll"
+        "${CMAKE_CURRENT_FUNCTION_LIST_DIR}/DirectX/Bin/x64/d3d12.dll"
         $<TARGET_FILE_DIR:${targetName}>)
 
-endfunction()
-
-function(UseDirectX11 targetName)
-
-	message(STATUS "${targetName}  include ${CMAKE_CURRENT_FUNCTION_LIST_DIR}/Include")
-	message(STATUS "${targetName} add lib ${CMAKE_CURRENT_FUNCTION_LIST_DIR}/Lib/x64/")
-
-	set(moduleName DirectX12)
-	ReMake_AddCustomModule(
-		TARGET_NAME ${targetName}
-		MODULE_NAME ${moduleName}
-		INC "${CMAKE_CURRENT_FUNCTION_LIST_DIR}/DirectX/Include"
-		LIB "${CMAKE_CURRENT_FUNCTION_LIST_DIR}/Lib/x64/dxgi.lib"
-			"${CMAKE_CURRENT_FUNCTION_LIST_DIR}/Lib/x64/d3d9.lib"
-			"${CMAKE_CURRENT_FUNCTION_LIST_DIR}/Lib/x64/d3d11.lib"
-			"${CMAKE_CURRENT_FUNCTION_LIST_DIR}/Lib/x64/dxguid.lib"
-			"${CMAKE_CURRENT_FUNCTION_LIST_DIR}/Lib/x64/d3dcompiler.lib"
-			"${CMAKE_CURRENT_FUNCTION_LIST_DIR}/Lib/x64/d3dx11.lib"
-			"${CMAKE_CURRENT_FUNCTION_LIST_DIR}/Lib/x64/dinput8.lib"
-			"${CMAKE_CURRENT_FUNCTION_LIST_DIR}/Lib/x64/X3DAudio.lib"
-			"${CMAKE_CURRENT_FUNCTION_LIST_DIR}/Lib/x64/xapobase.lib"
-			"${CMAKE_CURRENT_FUNCTION_LIST_DIR}/Lib/x64/XAPOFX.lib"
-		DEFINE "WITH_D3DX_LIBS=1"
-	)
+	UseDirectX11(${targetName})
 
 endfunction()
---
+++ src/windows/Use.cmake
@@ -0,0 +1,6 @@
+
+function(WithWinMain targetName)
+
+set_target_properties(${targetName} PROPERTIES WIN32_EXECUTABLE TRUE)
+
+endfunction()
