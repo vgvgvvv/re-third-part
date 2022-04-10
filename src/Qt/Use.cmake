97100deda92da628e4d08bcd04d6ca0d40a0afb9
ResetOTER <vgvgvvvdc@gmail.com>
2022-04-10 19:38:17 +0800

    Support Qt6

---
+++ src/Qt/Use.cmake
@@ -0,0 +1,20 @@
+
+
+macro(FindQtComponent ComponentName)
+
+    set(QtVersion 6)
+
+    #See if the environment var is set
+    if(DEFINED ENV{Qt${QtVersion}_HOME})
+        message(STATUS "Looking for Qt in: " $ENV{Qt${QtVersion}_HOME})
+    else()
+        message(STATUS "Qt${QtVersion}_HOME environment variable not set. Checking default paths.")
+    endif()
+
+    find_package(Qt${QtVersion}
+            COMPONENTS ${ComponentName}
+            REQUIRED PATHS "$ENV{Qt${QtVersion}_HOME}")
+
+    message(STATUS "!!! include QT include dir Qt${QtVersion}_INCLUDE_DIRS : ${Qt${QtVersion}_INCLUDE_DIRS}")
+
+endmacro()
